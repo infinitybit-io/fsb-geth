44027e58484a3feafec0e4d104a941cff73807f3
InfinityBit <128915987+infinitybit-io@users.noreply.github.com>
2023-03-26 02:56:09 -0700

    Create provision.sh

---
+++ provision.sh
@@ -0,0 +1,9 @@
+sudo apt install make snap
+sudo snap install go --classic
+git clone https://github.com/infinitybit-io/geth
+cd ./geth
+make geth
+mv ./build/bin/geth /usr/bin/geth
+mkdir -p ~/.infinitybit
+geth init --datadir ~/.infinitybit/fsb_data ./params/genesis.json
+cd ~
